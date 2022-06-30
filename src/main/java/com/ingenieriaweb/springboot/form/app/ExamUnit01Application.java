5f739a130747f68a89992ba9b54c7184ba0e498e
Richard Braul <richardzz@hotmail.com>
2022-06-29 20:29:38 -0500

    Inicio de Examen

---
+++ src/main/java/com/ingenieriaweb/springboot/form/app/ExamUnit01Application.java
@@ -0,0 +1,13 @@
+package com.ingenieriaweb.springboot.form.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamUnit01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamUnit01Application.class, args);
+	}
+
+}
