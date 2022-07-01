eac60d44b5064588822903b50cde2d7756d2aa3f
leodan12 <leomacip4@gmail.com>
2022-07-01 01:37:35 -0500

    con plantilla de inicio

--- src/main/java/com/ingenieriaweb/springboot/form/app/controllers/FormController.java
+++ src/main/java/com/ingenieriaweb/springboot/form/app/controllers/FormController.java
@@ -6,10 +6,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller // importo
-@RequestMapping("/app") // RUTAPRIMERNIVEL_importo, ruta de mi controlador que quiero mapear
+@RequestMapping({"/app","/"}) // RUTAPRIMERNIVEL_importo, ruta de mi controlador que quiero mapear
 public class FormController {
 
-	@GetMapping({ "/index", "/login", "/home" }) // importo-ruta entre llaves de index
+	@GetMapping({ "/index", "/login", "/home", "/" }) // importo-ruta entre llaves de index
 	public String index(Model model) {
 		model.addAttribute("titulo", "EXAMEN INGENIERIA WEB");
 		return "index";
