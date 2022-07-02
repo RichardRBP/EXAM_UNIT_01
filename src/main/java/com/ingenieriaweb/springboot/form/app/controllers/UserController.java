9d9c20647929d64e3dcde74345ea0be8ac4e41fc
Richard Braul <richardzz@hotmail.com>
2022-07-02 16:09:21 -0500

    Avance 02 Richard

---
+++ src/main/java/com/ingenieriaweb/springboot/form/app/controllers/UserController.java
@@ -0,0 +1,39 @@
+package com.ingenieriaweb.springboot.form.app.controllers;
+
+import javax.validation.Valid;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.ingenieriaweb.springboot.form.app.models.domain.Usuario;
+
+@Controller
+@RequestMapping({"/app","/",""})
+public class UserController {
+	
+	@GetMapping("/usuario")
+	public String usuarioIndex(Model model) {
+		Usuario usuario = new Usuario(null, null, null);
+		model.addAttribute("titulo", "Formulario de Usuarios");
+		model.addAttribute("usuario",usuario);
+		return "usuario/index";
+	}
+	
+	
+	@PostMapping("/app/usuario")
+	public String procesarUsuario(@Valid Usuario usuario,BindingResult result, Model model) {
+		 model.addAttribute("titulo", "Resultado Formulario Usuario");
+
+	        if(result.hasErrors()) {
+	            
+	            return "usuario/index";
+	        }
+	        model.addAttribute("usuario", usuario);
+			return "usuario/resultado";
+	}
+
+}
--- src/main/java/com/ingenieriaweb/springboot/form/app/models/domain/Usuario.java
+++ src/main/java/com/ingenieriaweb/springboot/form/app/models/domain/Usuario.java
@@ -2,42 +2,54 @@ package com.ingenieriaweb.springboot.form.app.models.domain;
 
 import java.util.List;
 
+import javax.validation.constraints.NotEmpty;
+
 public class Usuario {
 
-	//private String codigoUsuario;
+	@NotEmpty
 	private String nombre;
+
+	@NotEmpty
 	private String direccion;
+
+	@NotEmpty
 	private String telefono;
-	List<Prestamo>Item;
-	
-	
+
+	List<Prestamo> Item;
+
 	public Usuario(String nombre, String direccion, String telefono) {
 	}
+
 	public String getNombre() {
 		return nombre;
 	}
+
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
+
 	public String getDireccion() {
 		return direccion;
 	}
+
 	public void setDireccion(String direccion) {
 		this.direccion = direccion;
 	}
+
 	public String getTelefono() {
 		return telefono;
 	}
+
 	public void setTelefono(String telefono) {
 		this.telefono = telefono;
 	}
+
 	public List<Prestamo> getItem() {
 		return Item;
 	}
+
 	public void setItem(List<Prestamo> item) {
 		Item = item;
 	}
-	
-	
-	
+
 }
